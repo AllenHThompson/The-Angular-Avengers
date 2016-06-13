84c4634ecc06e38d7e6a17467f521f2104b525cf
Shanda Q Kennedy <shandakennedy@hotmail.com>
2016-06-13 16:52:20 -0400

    added the google map to the page

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,57 @@
 var app = angular.module('app',[]);
 
-  app.controller('JobSearch', function($scope, $http){
-    //debugger
-    var page = 0;
-    $http({
-      url: 'https://data.usajobs.gov/api/search',
-      params: {
-        JobCategoryCode: 2210,
-        PositionLocation: 'Atlanta, Ga'
-      },
-      headers: {
-        //'User-Agent': 'allenhthompson1@gmail.com',
-        'Authorization-Key': 'MfbLK4LehC6CQvAg3U9nr2Y0nBS5IHnMJjPK+KuoWbM='
-      }
-    }).success(function(data) {
-      debugger
-      $scope.resultList = data.SearchResult.SearchResultItems;
-      console.log(data);
-    });
+app.controller('JobSearch', function($scope, $http){
 
-    // $http.jsonp('https://jobs.github.com/positions.json?callback=JSON_CALLBACK',{
-    //   params:{
-    //     full_time: 'true',
-    //     title: 'web devolper',
-    //     // page: page,
-    //     location: 'sf, ca'
-    //   }
-    // }).success(function(data){
-    //
-    //   $scope.data = data;
-    //   //$scope.result = data.list;
-    //   console.log(data);
-    // });
-    });
+  // job search api call
+  $http({
+    url: 'https://data.usajobs.gov/api/search',
+    params: {
+      JobCategoryCode: 2210,
+      LocationName: 'Atlanta, Georgia'
+    },
+    headers: {
+      //'User-Agent': 'allenhthompson1@gmail.com',
+      'Authorization-Key': 'MfbLK4LehC6CQvAg3U9nr2Y0nBS5IHnMJjPK+KuoWbM='
+    }
+  }).success(function(data) {
+    $scope.resultList = data.SearchResult.SearchResultItems;
+    console.log(data);
+    //debugger
+  });
+
+
+  // google map api call
+  var centerLatLng = {lat: 33.7490, lng: 84.3880};
+
+  var mapOtions = {
+    center: centerLatLng,
+    zoom: 4
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), mapOtions);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+// $http.jsonp('https://jobs.github.com/positions.json?callback=JSON_CALLBACK',{
+//   params:{
+//     full_time: 'true',
+//     title: 'web devolper',
+//     // page: page,
+//     location: 'sf, ca'
+//   }
+// }).success(function(data){
+//
+//   $scope.data = data;
+//   //$scope.result = data.list;
+//   console.log(data);
+// });
