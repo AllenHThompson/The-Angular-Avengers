c4bcae038ad944677b923ec85c4ee0a6fbcce4c4
Allen H. Thompson <allenhthompson1@gmail.com>
2016-06-14 12:24:38 -0400

    Added Job Title to information window.

--- scripts/script.js
+++ scripts/script.js
@@ -14,11 +14,44 @@ app.controller('JobSearch', function($scope, $http){
                'Authorization-Key': 'MfbLK4LehC6CQvAg3U9nr2Y0nBS5IHnMJjPK+KuoWbM='
           }
      }).success(function(data) {
-          $scope.resultList = data.SearchResult.SearchResultItems;
-          console.log(data);
+          var allResultsList = data.SearchResult.SearchResultItems;
+
+          var filterGeorgiaResults = function(oneResult) {
+               var cityList = oneResult.MatchedObjectDescriptor.PositionLocation;
+               var gaLocations = cityList.filter(filterForGa);
+               function filterForGa(locationList){
+                    if(locationList.CountrySubDivisionCode === 'Georgia'){
+                         return true;
+                    } else
+                         return false;
+               }
+
+               if (gaLocations.length > 0) {
+                    return true;
+               } else
+                    return false;
 
 
-          var markers = $scope.resultList.map(function(job) {
+
+               /*
+               1. store locationList as a variable
+               2. filter locationList to only GA locations, store that in variable
+               3. if GA locations is empty (length of 0), return false, otherwise return true
+               */
+          };
+
+          function clearMarker() {
+               setMapOnAll(null);
+          }
+           $scope.georgiaResultsList = allResultsList.filter(filterGeorgiaResults);
+          // $scope.resultList = data.SearchResult.SearchResultItems;
+          console.log($scope.georgiaResultsList);
+
+
+
+
+          var markers = $scope.georgiaResultsList.map(function(job) {
+
                var locationList = job.MatchedObjectDescriptor.PositionLocation;
 
                //funtiont to fliter the list of jobs to only Georgia
@@ -28,9 +61,9 @@ app.controller('JobSearch', function($scope, $http){
                     }else
                          return false;
                }
-               var locationsInGeorgia = locationList.filter(filterForGa);
-               
-               locationsInGeorgia.map(function(location){
+                var locationsInGeorgia = locationList.filter(filterForGa);
+
+                 locationsInGeorgia.map(function(location){
                     var lat = location.Latitude;
                     var lng = location.Longitude;
                     var position = {
@@ -44,7 +77,8 @@ app.controller('JobSearch', function($scope, $http){
                          map: map,
 
                     });
-                    var contentString = '<a href =' + job.MatchedObjectDescriptor.PositionURI + '>Apply To This Job</a>';
+                    var contentString = '<a href =' + job.MatchedObjectDescriptor.PositionURI + '>Apply To This Job</a>' + '<h5>' + job.MatchedObjectDescriptor.PositionTitle + '</h5>';
+
 
                     var infowindow = new google.maps.InfoWindow({
                          content: contentString
@@ -68,7 +102,7 @@ app.controller('JobSearch', function($scope, $http){
 
      var mapOtions = {
           center: centerLatLng,
-          zoom: 10
+          zoom: 8
      };
 
      var map = new google.maps.Map(document.getElementById('map'), mapOtions);
