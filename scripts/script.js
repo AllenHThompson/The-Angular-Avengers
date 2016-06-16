dc55d5f8ce07245367818d4d828a2a7ad2353287
Allen H. Thompson <allenhthompson1@gmail.com>
2016-06-16 16:51:39 -0400

    Changed display on search page to show all cities.

--- scripts/script.js
+++ scripts/script.js
@@ -40,12 +40,7 @@ app.factory('weatherService', function($http){
   }; // end return
 }); // end app.factory(weatherService)
 
-function filterForGa(locationList){
-     if(locationList.CountrySubDivisionCode === 'Georgia'){
-          return true;
-     } else
-     return false;
-}
+
 
 // main controller
 app.controller('MainController', function($scope, jobSearchService, googleMap){
@@ -105,11 +100,24 @@ app.controller('JobSearch', function($scope, $http, $routeParams){
 
      // $scope.message = 'Test message.';//this line just checking connectivity
 
-     $scope.getGeorgiaLocation = function(job){
+     $scope.getLocation = function(job){
           var cityList = job.MatchedObjectDescriptor.PositionLocation;
-          var gaLocations = cityList.filter(filterForGa);
-          return gaLocations[0];
+          console.log(cityList);
+          var locations = cityList.filter(filterLocations);
+          console.log(locations);
+          return locations[0];
      };
+
+     function filterLocations(location){
+          if(location.LocationName.indexOf($routeParams.location) > -1){
+               return true;
+          } else if (location.LocationName.indexOf("Nationwide")){
+               return true;
+          } else if (location.LocationName.indexOf("Negotiable")){
+               return true;
+          }     
+          return false;
+     }
      $scope.openInfoWindow = function(job){
           infoWindow.setContent('<a target="_blank" href =' + job.MatchedObjectDescriptor.PositionURI + '>Apply To This Job</a>' + '<h5>' + job.MatchedObjectDescriptor.PositionTitle + '</h5>');
           infoWindow.open(map, job.marker);
@@ -128,23 +136,21 @@ app.controller('JobSearch', function($scope, $http, $routeParams){
           }
      }).success(function(data) {
           var allResultsList = data.SearchResult.SearchResultItems;
-          //console.log('data', allResultsList);
+          console.log('data', allResultsList);
 
-          var filterGeorgiaResults = function(oneResult) {
-               var cityList = oneResult.MatchedObjectDescriptor.PositionLocation;
-               var gaLocations = cityList.filter(filterForGa);
-
-
-               if (gaLocations.length > 0) {
-                    return true;
-               } else
-               return false;
-               /*
-               1. store locationList as a variable
-               2. filter locationList to only GA locations, store that in variable
-               3. if GA locations is empty (length of 0), return false, otherwise return true
-               */
-          };
+          // var filterGeorgiaResults = function(oneResult) {
+          //      var cityList = oneResult.MatchedObjectDescriptor.PositionLocation;
+          //      var gaLocations = cityList.filter(filterForGa);
+          //      if (gaLocations.length > 0) {
+          //           return true;
+          //      } else
+          //      return false;
+          //      /*
+          //      1. store locationList as a variable
+          //      2. filter locationList to only GA locations, store that in variable
+          //      3. if GA locations is empty (length of 0), return false, otherwise return true
+          //      */
+          // };
 
           function clearMarker() {
                setMapOnAll(null);
